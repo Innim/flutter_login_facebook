c50cd087e752df94bc7bd125967ae95565194047
greymag <greymag@gmail.com>
2020-11-05 18:17:15 +0300

    Example: express login usage.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login_facebook/flutter_login_facebook.dart';
@@ -7,14 +9,29 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   final plugin = FacebookLogin(debug: true);
 
   @override
-  _MyAppState createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHome(plugin: plugin),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class MyHome extends StatefulWidget {
+  final FacebookLogin plugin;
+
+  const MyHome({Key key, @required this.plugin})
+      : assert(plugin != null),
+        super(key: key);
+
+  @override
+  _MyHomeState createState() => _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> {
   String _sdkVersion;
   FacebookAccessToken _token;
   FacebookUserProfile _profile;
@@ -32,33 +49,36 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     final isLogin = _token != null && _profile != null;
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Login via Facebook example'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 18.0, horizontal: 8.0),
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                if (_sdkVersion != null) Text("SDK v$_sdkVersion"),
-                if (isLogin)
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: _buildUserInfo(context, _profile, _token, _email),
-                  ),
-                isLogin
-                    ? OutlineButton(
-                        child: Text('Log Out'),
-                        onPressed: _onPressedLogOutButton,
-                      )
-                    : OutlineButton(
-                        child: Text('Log In'),
-                        onPressed: _onPressedLogInButton,
-                      ),
-              ],
-            ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login via Facebook example'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 18.0, horizontal: 8.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              if (_sdkVersion != null) Text("SDK v$_sdkVersion"),
+              if (isLogin)
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: _buildUserInfo(context, _profile, _token, _email),
+                ),
+              isLogin
+                  ? OutlineButton(
+                      child: Text('Log Out'),
+                      onPressed: _onPressedLogOutButton,
+                    )
+                  : OutlineButton(
+                      child: Text('Log In'),
+                      onPressed: _onPressedLogInButton,
+                    ),
+              if (!isLogin && Platform.isAndroid)
+                OutlineButton(
+                  child: Text('Express Log In'),
+                  onPressed: () => _onPressedExpressLogInButton(context),
+                )
+            ],
           ),
         ),
       ),
@@ -102,6 +122,20 @@ class _MyAppState extends State<MyApp> {
       FacebookPermission.email,
     ]);
     _updateLoginInfo();
+  }
+
+  void _onPressedExpressLogInButton(BuildContext context) async {
+    final res = await widget.plugin.expressLogin();
+    if (res.status == FacebookLoginStatus.Success) {
+      _updateLoginInfo();
+    } else {
+      await showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          content: Text("Can't make express log in. Try regular log in."),
+        ),
+      );
+    }
   }
 
   void _onPressedLogOutButton() async {
