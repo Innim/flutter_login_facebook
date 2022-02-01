2764755662c98ff92d062c054b93a4dff50c4220
greymag <greymag@gmail.com>
2022-02-01 12:43:38 +0300

    Remove all not site related files.

--- android/src/main/java/ru/innim/flutter_login_facebook/ErrorCode.java
+++
@@ -1,8 +0,0 @@
-package ru.innim.flutter_login_facebook;
-
-public class ErrorCode {
-    public static final String UNKNOWN = "UNKNOWN";
-    public static final String INTERRUPTED = "INTERRUPTED";
-    public static final String FAILED = "FAILED";
-    public static final String INVALID_ARGS = "INVALID_ARGS";
-}
--- lib/flutter_login_facebook.dart
+++
@@ -1,8 +0,0 @@
-library flutter_login_facebook;
-
-export './src/facebook_login.dart';
-export './src/models/facebook_user_profile.dart';
-export './src/models/facebook_access_token.dart';
-export './src/models/facebook_login_result.dart';
-export './src/models/facebook_error.dart';
-export './src/models/facebook_permission.dart';
