5f2bcda25906dacbfd7e2ada932429c89d701acf
greymag <greymag@gmail.com>
2022-02-28 17:08:38 +0300

    Commented test example.

--- test/flutter_login_facebook_test.dart
+++ test/flutter_login_facebook_test.dart
@@ -1,20 +1,17 @@
-import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-
 void main() {
-  const channel = MethodChannel('flutter_login_facebook');
+  // const channel = MethodChannel('flutter_login_facebook');
 
-  TestWidgetsFlutterBinding.ensureInitialized();
+  // TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
-    channel.setMockMethodCallHandler((methodCall) async {
-      return '42';
-    });
-  });
+  // setUp(() {
+  //   channel.setMockMethodCallHandler((methodCall) async {
+  //     return '42';
+  //   });
+  // });
 
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
+  // tearDown(() {
+  //   channel.setMockMethodCallHandler(null);
+  // });
 
   // test('getPlatformVersion', () async {
   //   expect(await FlutterLoginFacebook.platformVersion, '42');
